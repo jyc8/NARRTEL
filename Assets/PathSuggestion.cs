72f5c520d04ab011d02025a7b34f3396d30cc2de
Jason <jyc8@live.com>
2015-04-24 04:33:18 -0700

    huh

---
+++ Assets/PathSuggestion.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathSuggestion : MonoBehaviour {
+
+	public GameObject thisObj;
+	public GameObject eventsFolder;
+	
+	public void showSuggestion(){
+		//Debug.Log ("entered");
+		GameObject selection = this.transform.GetChild(4).gameObject;
+		selection.SetActive (true);
+	}
+	
+	public void hideSuggestion(){
+		//Debug.Log ("exited");
+		GameObject selection = this.transform.GetChild(4).gameObject;
+		selection.SetActive (false);
+	}
+	
+	public void delete(){
+		Destroy (thisObj);
+	}
+	
+	public void applySuggestion(){
+		while (thisObj.transform.GetChild(4).transform.childCount > 0) {
+			thisObj.transform.GetChild(4).transform.GetChild(0).parent = eventsFolder.transform;
+		}
+		Destroy (thisObj);
+	}
+}
